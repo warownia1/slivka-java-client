58c75372e6a25a608ef197133a7703d7b0c1893d
MMWarowny <mmzwarowny@dundee.ac.uk>
2019-11-04 16:02:27 +0000

    Add missing job states to the enum.

--- src/main/java/uk/ac/dundee/compbio/slivkaclient/JobState.java
+++ src/main/java/uk/ac/dundee/compbio/slivkaclient/JobState.java
@@ -2,9 +2,13 @@ package uk.ac.dundee.compbio.slivkaclient;
 
 public enum JobState {
   PENDING,
+  REJECTED,
+  ACCEPTED,
   QUEUED,
   RUNNING,
   COMPLETED,
+  INTERRUPED,
+  DELETED,
   FAILED,
   ERROR,
   UNKNOWN
