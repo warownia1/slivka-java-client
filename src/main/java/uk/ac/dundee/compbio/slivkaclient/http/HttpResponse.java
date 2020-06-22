2fd9b4d9aabb0f99f5ec9285dcbbd0bb968b9fd5
Mateusz Warowny <mmzwarowny@dundee.ac.uk>
2020-06-22 16:50:09 +0100

    add "throws" to HttpResposne methods that may raise IOException

--- src/main/java/uk/ac/dundee/compbio/slivkaclient/http/HttpResponse.java
+++ src/main/java/uk/ac/dundee/compbio/slivkaclient/http/HttpResponse.java
@@ -1,14 +1,15 @@
 package uk.ac.dundee.compbio.slivkaclient.http;
 
 import java.io.Closeable;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Map;
 
 public interface HttpResponse extends Closeable {
   public int getStatusCode();
   public Map<String, String> getHeaders();
-  public InputStream getContent();
-  public String getText();
+  public InputStream getContent() throws IOException;
+  public String getText() throws IOException;
   @Override
-  public default void close() {}
+  public default void close() throws IOException {}
 }
